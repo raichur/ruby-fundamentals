3f97eb958c521525e39ea4e60132ef75641159fe
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 17:23:44 +0530

    Strings

---
+++ string-special-chars.rb
@@ -0,0 +1,5 @@
+str1 = "\a" # Here you actually get the alarm character, not the literal value, but the interpreted value
+str2 = '\a' # Passed on 'literally' by single quoted strings
+
+puts str1
+puts str2
---
+++ strings.rb
@@ -0,0 +1,5 @@
+str1 = "This is a string" # Double quote (no difference)
+str2 = 'This is also a string' # Single quote (no difference)
+
+puts str1
+puts str2

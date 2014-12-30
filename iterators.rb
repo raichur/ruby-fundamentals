d33c540e100755e3874989aa9f8c91d406a16457
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 19:00:13 +0530

    Iterators start. Just wanted to do 50 commits today :smirk:

---
+++ iterators.rb
@@ -0,0 +1,13 @@
+arr = [10, 15, 20, 25, 30, 35, 40]
+
+# Iterate over array:
+arr.each do |val|
+  puts val
+end
+
+puts
+
+# Iterate over array (alternate syntax)
+arr.each{|val| puts val}
+
+# Operative statement can be more complex

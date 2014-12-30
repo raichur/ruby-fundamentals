c45d87b2537a79fe7fe53500b88f304c0af1097b
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 17:26:38 +0530

    string interpolation

---
+++ string-interpolation.rb
@@ -0,0 +1,20 @@
+x = 12
+name = "Josh"
+
+str1 = "x has value #{x} and has value #{name}"
+str2 = 'x has value #{x} and has value #{name}'
+
+puts "Double quotes:"
+puts str1
+puts "Single quotes:"
+puts str2
+
+puts
+
+str3 = "x + 2 = #{x + 2}"
+str4 = 'x + 2 = #{x + 2}'
+
+puts "Double quotes:"
+puts str3
+puts "Single quotes:"
+puts str4

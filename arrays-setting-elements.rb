5beeec635d2cea6bb75e271cf51781b72dd9d99e
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 18:18:48 +0530

    Array setting elements

---
+++ arrays-setting-elements.rb
@@ -0,0 +1,25 @@
+arr = [10, 20, 30, 40, 50]
+
+arr[0] = -10
+puts arr
+
+puts
+puts "************"
+puts
+
+arr[-2] = -40 # Changes the vaule (counting backwards) to the value that was 40 to -40
+puts arr
+
+puts
+puts "************"
+puts
+
+arr[1, 2] = -20, -30
+puts arr
+
+puts
+puts "************"
+puts
+
+arr[0..3] = 1, 1, 1, 1
+puts arr

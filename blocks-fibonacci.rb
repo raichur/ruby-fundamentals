ea8cb97b0dc9e0c748fed4acb4ab5d8fa4410cb4
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 21:06:16 +0530

    Fib sequence blocks

---
+++ blocks-fibonacci.rb
@@ -0,0 +1,14 @@
+# Fibonacci sequence is 1, 1, 2, 3, 5, 8, ... where the first two elements are 1,
+# and each successive element is the sum of the previous two elements
+
+def fib_to(max)
+  i1, i2 = 1, 1
+  while i1 <= max
+    yield i1
+    i1, i2 = i2, i1 + i1
+  end
+end
+
+fib_to(15) do |f|
+  puts f
+end

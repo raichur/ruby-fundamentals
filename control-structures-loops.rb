f24fbd18a0d8d2e2ab923130040bfc13ca4092e4
Joshua Raichur <hello@joshuaraichur.com>
2014-12-30 18:56:34 +0530

    loops

---
+++ control-structures-loops.rb
@@ -0,0 +1,24 @@
+x = 3
+
+# While loop
+puts 'Until Loop:'
+while x < 10
+  puts x
+  x += 1
+end
+puts
+
+x = 3
+# Until loop
+puts 'Until Loop:'
+until x >= 10
+  puts x
+  x += 1
+end
+puts
+
+# Times loop
+puts 'Times Loop:'
+3.times do
+  puts 'I say this thrice'
+end

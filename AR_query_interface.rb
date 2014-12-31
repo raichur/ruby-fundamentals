8b39c58d577a752f2081b95e8b99de3cf37db074
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 07:29:27 +0530

    Ar query interface

---
+++ AR_query_interface.rb
@@ -0,0 +1,21 @@
+u = User.find(1)
+
+u = User.find(123149503)
+
+u = User.first
+
+w = Work.first(3)
+
+p = Project.last(2)
+
+u = User.find_by fname: 'Josh'
+
+w = Work.find_by hours: 6
+
+fulldayworkitems = Work.where('hours = ?', 8)
+
+lowdefaultrateprojects = Project.where('default_rate < ?', 100)
+
+companies = Company.order('name')
+
+companies = Company.order('name desc')
